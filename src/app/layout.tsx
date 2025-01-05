140488af709dfef9a0ac07bf2523fda5afdf3a50
William-Cossa <devwilliamcossa@gmail.com>
2025-01-06 00:09:34 +0200

    Texto pt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <Providers>
-        <html lang="en">
+        <html lang="pt-br">
           <body className={inter.className}>
             {children}
             <Toaster />
